f38c1757a5d68fcda99662c42242a1f387e1d503
syro13 <lowisjakub@gmail.com>
2023-02-15 15:10:22 +0000

    files updated

---
+++ page-1.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.80.0">
+    <title>Starter Template · Bootstrap v5.0</title>
+    <!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="mystyle.css" rel="stylesheet">
+  </head>
+  <body>
+    
+  <?php include 'includes/header.php';?>
+<main class="container">
+  <div class="starter-template text-center">
+    <h1>Page 1</h1>
+    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+  </div>
+
+</main><!-- /.container -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
